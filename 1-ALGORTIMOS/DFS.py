639f57e4bdb8a5de1b677363c71a9a0222d85ced
D1EGX10 <dmendieta2005@gmail.com>
2026-03-06 20:03:22 -0600

    DFS-4

--- 1-ALGORTIMOS/DFS.py
+++ 1-ALGORTIMOS/DFS.py
@@ -2,13 +2,19 @@ def dfs(grafo, inicio, objetivo):
     pila = [inicio]
     visitados = set()
     camino = {inicio: None}
-def dfs(grafo, inicio, objetivo):
-    pila = [inicio]
-    visitados = set()
-    camino = {inicio: None}
 
     while pila:
         nodo = pila.pop()
 
         if nodo not in visitados:
             visitados.add(nodo)
+
+            if nodo == objetivo:
+                return camino
+
+            for vecino in grafo[nodo]:
+                if vecino not in visitados:
+                    pila.append(vecino)
+
+                    if vecino not in camino:
+                        camino[vecino] = nodo
